2582eac94d360cfd6548653fb2999596cec74e6b
daoptph23912 <thanhdao02dd@gmail.com>
2025-12-23 20:55:40 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -34,13 +34,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/health", healthRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 if (process.env.AUTO_SEED === "true") {
   seedDatabase();
 }
